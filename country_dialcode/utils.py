8ed0df601940bf4ef258be3dc733b24ad9e9f473
areski <areski@gmail.com>
2014-07-16 14:35:09 +0200

    fix tests

--- country_dialcode/utils.py
+++ country_dialcode/utils.py
@@ -1,54 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib.auth.models import User
-from django.test import TestCase, Client
-from django.test.client import RequestFactory
-import base64
-import unittest
 import inspect
-
-
-def build_test_suite_from(test_cases):
-    """Returns a single or group of unittest test suite(s) that's ready to be
-    run. The function expects a list of classes that are subclasses of
-    TestCase.
-
-    The function will search the module where each class resides and
-    build a test suite from that class and all subclasses of it.
-    """
-    test_suites = []
-    for test_case in test_cases:
-        mod = __import__(test_case.__module__)
-        components = test_case.__module__.split('.')
-        for comp in components[1:]:
-            mod = getattr(mod, comp)
-        tests = []
-        for item in mod.__dict__.values():
-            if type(item) is type and issubclass(item, test_case):
-                tests.append(item)
-        test_suites.append(unittest.TestSuite(
-            map(unittest.TestLoader().loadTestsFromTestCase, tests)))
-
-    return unittest.TestSuite(test_suites)
-
-
-class BaseAuthenticatedClient(TestCase):
-    """Common Authentication"""
-    fixtures = ['auth_user.json']
-
-    def setUp(self):
-        """To create admin user"""
-        self.client = Client()
-        self.user = User.objects.get(username='admin')
-        auth = '%s:%s' % ('admin', 'admin')
-        auth = 'Basic %s' % base64.encodestring(auth)
-        auth = auth.strip()
-        self.extra = {
-            'HTTP_AUTHORIZATION': auth,
-        }
-        login = self.client.login(username='admin', password='admin')
-        self.assertTrue(login)
-        self.factory = RequestFactory()
 
 
 class Choice(object):
