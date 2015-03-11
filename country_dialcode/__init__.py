fd570196fd6705bee78b68ad0a5683a6744059b9
areski <areski@gmail.com>
2015-03-11 22:51:58 +0100

    Update to version v0.6.0

--- country_dialcode/__init__.py
+++ country_dialcode/__init__.py
@@ -7,7 +7,7 @@
 # :license:   MIT, see MIT-LICENSE.txt for more details.
 #
 
-__version__ = '0.5.1'  # edit also docs/source/conf.py and update requirements.txt
+__version__ = '0.6.0'  # edit also docs/source/conf.py and update requirements.txt
 __author__ = "Arezqui Belaid"
 __contact__ = "areski@gmail.com"
 __homepage__ = "http://www.areskibelaid.com"
